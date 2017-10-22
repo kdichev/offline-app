9484bc04cb22812533cd0c1a81bd81d482edf3de
Konstantin-home <konstantin.dichev@gmail.com>
2017-10-22 19:20:14 +0200

    change test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {App} from './App';
 import { Provider } from 'react-redux'
-const store = {
-  reducer: {
-    online: false
-  }
-}
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App store={store}/>, div);
+  ReactDOM.render(<App />, div);
 });
