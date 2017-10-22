90f11494247226e780bc13111e285e21a2f6cdc9
Konstantin-home <konstantin.dichev@gmail.com>
2017-10-22 19:01:41 +0200

    add persisted state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { loadState, saveState } from './localStorage'
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
 
@@ -12,12 +13,19 @@ const appStore = combineReducers({
   reducer
 });
 
+const persistedState = loadState();
+
 let store = createStore(
   appStore,
+  persistedState,
   process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     : undefined
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+})
 
 ReactDOM.render(
   <Provider store={store}>
